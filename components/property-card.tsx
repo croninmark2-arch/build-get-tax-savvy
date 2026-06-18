f8a8cd7c4ae6eab42d171a01f1ba9f5890df20e3
croninmark2-arch <cronin.mark2@gmail.com>
2026-06-17 20:59:57 -0400

    Add default accent color to PropertyCard

--- components/property-card.tsx
+++ components/property-card.tsx
@@ -58,7 +58,7 @@ function money(n: number) {
 export function PropertyCard({
   property,
   onChange,
-}: {
+const accent = categoryColors[property.category] || "bg-gray-50 border-l-4 border-gray-500"
   property: Property
   onChange: (next: Property) => void
 }) {
